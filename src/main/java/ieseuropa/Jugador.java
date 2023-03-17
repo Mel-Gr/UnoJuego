3e67db7e437ab87bb4d29bbcbea917d2bbd0ad0a
dev <dev@debian>
2023-03-17 20:04:42 +0100

    primer commit

---
+++ src/main/java/ieseuropa/Jugador.java
@@ -0,0 +1,5 @@
+package main.java.ieseuropa;
+
+public class Jugador {
+
+}
---
+++ src/main/java/ieseuropa/Start.java
@@ -0,0 +1,10 @@
+package main.java.ieseuropa;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
