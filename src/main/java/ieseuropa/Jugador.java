24f2fe528e499ff7fa17dc742381ea02c95ff42b
Mel-Gr <dev@debian>
2023-03-17 20:32:46 +0100

    Añadir dos metodos a Jugador

--- src/main/java/ieseuropa/Jugador.java
+++ src/main/java/ieseuropa/Jugador.java
@@ -2,4 +2,20 @@ package main.java.ieseuropa;
 
 public class Jugador {
 
+	
+	
+	
+	
+	
+	
+	
+	public void empezarTurno () {
+		if (!turno)
+			turno = true;
+	}
+	
+	public void terminarTurno () {
+		if (turno)
+			turno = false;
+	}
 }
